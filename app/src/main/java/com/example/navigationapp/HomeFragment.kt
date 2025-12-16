2919831b03d5ae6ce525e2529ced9659829c27f6
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2025-12-16 09:51:26 +0100

    Update HomeFragment.kt

--- app/src/main/java/com/example/navigationapp/HomeFragment.kt
+++ app/src/main/java/com/example/navigationapp/HomeFragment.kt
@@ -8,14 +8,10 @@ import android.view.ViewGroup
 import com.example.navigationapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }

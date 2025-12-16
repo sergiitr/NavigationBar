e9a7b6d1e34e96f74f132cdef6cfdde96c1138cc
Sergio Trillo Rodriguez <86511887+sergiitr@users.noreply.github.com>
2025-12-16 09:48:54 +0100

    Update HospedajeFragment.kt

--- app/src/main/java/com/example/navigationapp/HospedajeFragment.kt
+++ app/src/main/java/com/example/navigationapp/HospedajeFragment.kt
@@ -7,14 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 
 class HospedajeFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
         // Inflamos la vista usando el método tradicional (R.layout.fragment_hospedaje)
         val view = inflater.inflate(R.layout.fragment_hospedaje, container, false)
-
         return view
     }
 }
